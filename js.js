95c896f8599c33d5d45ebf3a31bdc4b5f589355e
Konrad Szymanski <46305261+konradszymanski@users.noreply.github.com>
2021-07-05 18:43:59 +0100

    added manual, readme, delete instruction on livepage

--- js.js
+++ js.js
@@ -1,7 +1,6 @@
 
   document.querySelectorAll('.hamburger-menu').forEach(hamburger => {
     hamburger.addEventListener('click', function() {
-        //hamburger.style.backgroundColor = "gray";
         hamburger.classList.toggle("rotHmb");
     })
   })
